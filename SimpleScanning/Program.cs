c277807aa3a186344b152631a906ff2cebaf1e70
Zachary Allen <zachary.thomas.allen@outlook.com>
2020-02-07 11:49:51 -0500

    Version 1.1, Add ID Numbers for Events, Organize Code

--- SimpleScanning/Program.cs
+++ SimpleScanning/Program.cs
@@ -11,44 +11,282 @@ namespace SimpleScanning
 {
     class Program
     {
-        static void Main(string[] args)
+        // ID 001
+        static void Main(string[] args) 
         {
-            EditConsole();
+            DefaultConsole(); 
             OnLoad();
             ShowMenu();
-            MainMenu();
+            DisplayList();
         }
 
-        public static void EditConsole()
-        {
-            Console.SetWindowSize(150, 45);
-            Console.Title = "SIMPLE SCANNING! press <ctrl+c> to exit at any time";
-            EditColor();
-            Console.Clear();
-        }
-
-        public static void EditColor() // For Terminal Default
-        {
-            Console.BackgroundColor = ConsoleColor.Gray;
-            Console.ForegroundColor = ConsoleColor.Black;
-        }
-
-        public static void OnLoad()
+        public static void OnLoad() // Menu On Load 
+        // ID 002
         {
             string welcome1 = "Welcome to Simple Scanning!\r\n";
             string welcome2 = "Press any key to continue...\r\n";
             CenterText(welcome1);
             CenterText(welcome2);
             Console.CursorVisible = false;
+            Console.Read();
+            Console.Clear(); // Clears text after keyboard press
         }
-        public static void ShowMenu()
+
+        public static void DisplayList() 
+        // ID 003
+        {
+            // establish variables
+            bool programRun = true;
+            bool turnPage = false;
+            int pageNum = 1;
+            int selectedIndex = 0;
+            ArrayList taskItems = new ArrayList();
+
+            do 
+        // ID 003.001
+            {
+                Console.Clear(); // Clear Console on loop
+                ShowMenu(); // Bottom display of avalible key pressess
+                Console.SetCursorPosition(0, 4);
+                PrintList(taskItems, selectedIndex, pageNum); // Prints current list
+                ConsoleKey keyPress = Console.ReadKey(true).Key;
+
+                switch (keyPress) 
+        // ID 003.001.001
+                {
+
+                    case ConsoleKey.A: // Add 
+        // ID 003.001.001.001
+                        KeyEvent(taskItems, 1, selectedIndex, pageNum);
+                        continue;
+
+                    case ConsoleKey.F: // Finish 
+        // ID 003.001.001.002
+                        KeyEvent(taskItems, 2, selectedIndex, pageNum);
+                        continue;
+
+                    case ConsoleKey.R: // Reenter
+        // ID 003.001.001.003
+                        KeyEvent(taskItems, 3, selectedIndex, pageNum);
+                        continue;
+
+                    case ConsoleKey.S: // Skip 
+        // ID 003.001.001.004
+                        KeyEvent(taskItems, 4, selectedIndex, pageNum);
+                        continue;
+
+                    case ConsoleKey.DownArrow: 
+        // ID 003.001.001.005
+                        turnPage = KeyEvent(taskItems, 5, selectedIndex, pageNum);
+                        if (turnPage)
+                        {
+                            pageNum++;
+                        }
+                        continue;
+
+                    default: 
+        // ID 003.001.001.006
+                        break;
+                }
+            } while (programRun);
+        }
+
+        public static void PrintList(ArrayList taskItems, int selectedIndex, int pageNum) 
+        // ID 004
+        {
+            int maxNum = pageNum * 20 - 1;
+
+            if (taskItems.Count == 0) // If list is empty 
+        // ID 004.001
+            {
+                CenterText("-- No Tasks --");
+            }
+
+            else if (pageNum == 1) 
+        // ID 004.002
+            {
+                var (indexOutput, indexLength) = ObjectToString(taskItems, selectedIndex);
+
+                if (selectedIndex == 0) // If selectedIndex is [0] 
+        // ID 004.002.001
+                {
+                    SelectProcess(indexOutput, indexLength); // Chagnes color of selectedIndex
+
+                    if (taskItems.Count > 1) // Prints all list items regularly after [i]
+        // ID 004.002.001.001
+                    {
+
+                        for (int i = selectedIndex; i <= maxNum; i++) 
+        // ID 004.002.001.001.001
+                        {
+                            var (internalOutput, _) = ObjectToString(taskItems, selectedIndex);
+                            CenterText(internalOutput);
+                        }
+                    }
+                }
+
+                else if(selectedIndex > 0) // If selectedIndex [i] > 0 
+        // ID 004.002.002
+                {
+
+                    for (int i = 0; i < selectedIndex + 1; i++) // Prints all list items regularly before [i]
+        // ID 004.002.002.001
+                    {
+                        CenterText(taskItems[i].ToString());
+                    }
+                    SelectProcess(indexOutput, indexLength);  // Chagnes color of selectedIndex
+
+                    for (int i = selectedIndex + 1; i <= 20; i++) // Prints all list items regularly after [i] 
+        // ID 004.002.002.002
+                    {
+                        CenterText(taskItems[i].ToString());
+                    }
+                }
+
+                else 
+        // ID 004.002.003
+                {
+                    CenterText(" ");
+                }
+            }
+
+            else // Handles Page Flip 
+        // ID 004.003
+            {
+                var (indexOutput, indexLength) = ObjectToString(taskItems, selectedIndex);
+
+                for (int i = 0; i < selectedIndex; i++) // Prints all list items regularly before [i]
+        // ID 004.003.001
+                {
+                    CenterText(taskItems[i].ToString());
+                }
+                SelectProcess(indexOutput, indexLength);  // Chagnes color of selectedIndex
+
+                for (int i = selectedIndex + 1; i < taskItems.Count; i++) // Prints all list items regularly after [i]
+        // ID 004.003.002
+                {
+                    CenterText(taskItems[i].ToString());
+                }
+            }
+        }
+
+        public static bool KeyEvent(ArrayList taskItems, int ID, int selectedIndex, int pageNum) 
+        // ID 005
+        {
+            bool turnPage = false;
+            int pageCondition = selectedIndex % 20;
+            int maxNum = pageNum * 20 - 1;
+            int i = maxNum - 20;
+
+            if (ID == 1) // Add
+        // ID 005.001
+            {
+                CenterText("Enter new task: ");
+                Console.CursorVisible = true;
+                var input = Console.ReadLine();
+                taskItems.Add(input);
+            }
+
+            else if (ID == 2) // Finish
+        // ID 005.002
+            {
+
+            }
+
+            else if (ID == 3) // ReEnter
+        // ID 005.003
+            {
+                taskItems.Add(taskItems[selectedIndex]);
+            }
+
+            else if (ID == 4) // Skip
+        // ID 005.004
+            {
+                //SelectColor(text, size, 2);
+            }
+
+            else if (ID == 5) // Down Arrow
+        // ID 005.005
+            {
+                
+                if (selectedIndex >= 0 || selectedIndex <= 19)
+        // ID 005.005.001
+                {
+                    selectedIndex++;
+                }
+
+                else if (pageCondition == 0) // If pageNum %20, print new page
+        // ID 005.005.002
+                {
+                    selectedIndex++;
+                    turnPage = true;
+                    return turnPage;
+                }
+                return turnPage;
+            }
+            return turnPage;
+        }
+
+        public static void DefaultConsole() // Changes Console Default 
+        // ID 006
+        {
+            Console.SetWindowSize(150, 45);
+            Console.Title = "SIMPLE SCANNING! press <ctrl+c> to exit at any time";
+            EditColor(1);
+            Console.Clear();
+        }
+
+        public static void EditColor(int ID) // For Color Changes 
+        // ID 007
+        {
+
+            if (ID == 1) // Default Color
+        // ID 007.001
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+
+            else if (ID == 2) // Selected Color
+        // ID 007.002
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            else // Cleans Up Selected Color; pass indexLength as ID
+        // ID 007.003
+            {
+                int y = Console.CursorTop;
+                Console.SetCursorPosition(0, y);
+                string spaces = "  ";
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.WriteLine(String.Format("{0," + (((Console.WindowWidth - ID) / 2) + (spaces.Length / 3)) + "}", spaces));
+            }
+        }
+
+        public static void SelectProcess(string indexOutput, int indexLength) 
+        // ID 008
+        {
+            int x = Console.CursorLeft;
+            int y = Console.CursorTop;
+            EditColor(2);
+            CenterText(indexOutput += " ");
+            EditColor(indexLength);
+            Console.SetCursorPosition(x, y + 1);
+            EditColor(1);
+        }
+
+        public static void ShowMenu() // Always show keyboard shortcuts at bottom
+        // ID 009
         {
 
             var text1 = "============================================================";
             var text2 = "DownArrow = Next : " +
                         "S = Skip : " +
                         "A = Add : " +
-                        "R = Renter : ";
+                        "R = Renter : " +
+                        "F = Finsih ";
             int x = Console.CursorLeft;
             int y = Console.CursorTop;
             Console.CursorTop = Console.WindowTop + Console.WindowHeight - 5;
@@ -58,166 +296,55 @@ namespace SimpleScanning
             Console.SetCursorPosition(x, y);
         }
 
-        public static void MainMenu() // Currently Working On
+        public static string CenterText(string text) // Console.WriteLine(Center) 
+        // ID 010
         {
-            bool programRun = true;
-            ConsoleKey choice;
-            ArrayList taskItems =
-                new ArrayList();
-            int selected = 0;
-            int Page = 1;
-            var (text, size) = ObjStr(taskItems, selected);
-
-            do
-            {
-                Console.Clear();
-                OnLoad();
-                ShowMenu();
-                int n = taskItems.Count;
-                PntFst(taskItems, selected);
-                (text, size) = ObjStr(taskItems, selected);
-
-                choice = Console.ReadKey(true).Key;
-                switch (choice)
-                {
-                    case ConsoleKey.A: // Add
-                        CenterText("Enter new task: ");
-                        Console.CursorVisible = true;
-                        var input = Console.ReadLine();
-                        taskItems.Add(input);
-                        continue;
-                    case ConsoleKey.S: // Skip
-                        // chagne color to red
-                        SelectColor(text, size, 2);
-                        continue;
-                    case ConsoleKey.R: // Reenter
-                        taskItems.Add(taskItems[selected]);
-                        continue;
-                    case ConsoleKey.DownArrow:
-                        if (selected >= 0 && selected <= taskItems.Count || 
-                            selected < 20 && selected <= taskItems.Count)
-                        {
-                            selected++;
-                        }
-                        if (selected == 20)
-                        {
-                            Page++;
-                            
-                            selected++;
-                        }
-                        if (selected == taskItems.Count)
-                        {
-                            
-                        }
-                        continue;
-                    default:
-                        break;
-                }
-                
-            } while (programRun);
-        }
-
-
-        public static string CenterText(string text)
-        {
-            Console.WriteLine(String.Format("{0," 
-            + ((Console.WindowWidth / 2) 
-            + (text.Length / 2)) + "}", text));
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (text.Length / 2)) + "}", text));
             return text;
         }
 
-        public static void PntFst(ArrayList taskItems, int selected)
+        public static (string, int) ObjectToString(ArrayList taskItems, int desiredIndex) 
+        // ID 011
         {
-            if (taskItems.Count == 0)
-            {
-                CenterText("No Tasks");
-            }
-            else
-            {
-                var (text, size) = ObjStr(taskItems, selected);
-
-                if (selected == 0)
-                {
-                    SelectColor(text, size, 1);
-                    if (taskItems.Count > 1)
-                    {
-                        for (int i = selected + 1; i < taskItems.Count; i++)
-                        {
-                            CenterText(taskItems[i].ToString());
-                        }
-                    }
-                
-                }
-                else if(selected > 0)
-                {
-                    
-                    for (int i = 0; i < selected; i++)
-                    {
-                        CenterText(taskItems[i].ToString());
-                    }
-                    SelectColor(text, size, 1);
-                    for (int i = selected + 1; i < taskItems.Count; i++)
-                    {
-                        CenterText(taskItems[i].ToString());
-                    }
-
-                }
-                CenterText(" ");
-            }
+            string indexOutput = taskItems[desiredIndex].ToString();
+            int indexLength = indexOutput.Length + 4;
+            return (indexOutput, indexLength);
         }
 
-        public static (string, int) ObjStr(ArrayList taskItems, int selected)
-        {
-            string text = taskItems[selected].ToString();
-            int size = text.Length + 4;
-            return (text, size);
-        }
-        public static void SelectColor(string text, int size, int ID) // For Selected Text
-        {
-            // Sets variables, saves cursor location
-            int x = Console.CursorLeft;
-            int y = Console.CursorTop;
-            
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.White;
-            text += " ";
-            CenterText(text);
-            CleanUp(size);
-            Console.SetCursorPosition(x, y + 1);
-            EditColor();
-        }
+        //--------------------------------------------------------------------------------------------------------
+        //                                       Version 1 Snippets Removed
+        //--------------------------------------------------------------------------------------------------------
 
-        public static void CleanUp(int size)
-        {
-            int y = Console.CursorTop;
-            Console.SetCursorPosition(0, y);
-            string spaces = "  ";
-            Console.BackgroundColor = ConsoleColor.Gray;
-            Console.WriteLine(String.Format("{0,"
-                + (((Console.WindowWidth - size) / 2)
-                + (spaces.Length / 3)) + "}", spaces));
-        }
+        //public static void SelectColor(string text, int size, int ID) // For Selected Text
+        //{
+
+        //}
+
+
+        //--------------------------------------------------------------------------------------------------------
+        //                                   UnNeeded
+        //--------------------------------------------------------------------------------------------------------
 
         //public static void Selected(ArrayList taskList, int selected)
         //{
         //    string text = taskList[selected].ToString();
-       //     int length = text.Length + 4;
-       //     Console.CursorVisible = true;
-       // }
+        //     int length = text.Length + 4;
+        //     Console.CursorVisible = true;
+        // }
 
 
-       // public static int HighlightLength(LinkedList<string> taskList)
-       // {
+        // public static int HighlightLength(LinkedList<string> taskList)
+        // {
 
         //    var temp = taskList.ElementAt(n);
-       // }
+        // }
         // Move Cursor
         //Index -> color
         //    select = 0
-       //     down key if 0 then add 1, if 20, change page 1 to page 2, move to 21, repeat, if end move to 0
+        //     down key if 0 then add 1, if 20, change page 1 to page 2, move to 21, repeat, if end move to 0
 
 
-           
+
         // Count += 1;
         // Count %= list.Count;
 
